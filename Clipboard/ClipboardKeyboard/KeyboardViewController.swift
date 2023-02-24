39b2b51213727227fdaee9d2aedfb0c551ac274e
ybwdaisy <ybw_daisy@163.com>
2023-02-24 17:41:21 +0800

    styles

--- Clipboard/ClipboardKeyboard/KeyboardViewController.swift
+++ Clipboard/ClipboardKeyboard/KeyboardViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 import SwiftUI
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
-    
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
@@ -22,19 +19,6 @@ class KeyboardViewController: UIInputViewController {
         super.viewDidLoad()
         
         // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
-        
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        self.nextKeyboardButton.isHidden = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
         let scrollView = self.addKeyboardButtons()
         
@@ -47,36 +31,6 @@ class KeyboardViewController: UIInputViewController {
         
         self.view.addSubview(mainStackView)
         
-        self.view.addConstraints([
-            NSLayoutConstraint(item: mainStackView,
-                attribute: .centerX,
-                relatedBy: .equal,
-                toItem: self.view,
-                attribute: .centerX,
-                multiplier: 1.0,
-                constant: 0.0),
-            NSLayoutConstraint(item: mainStackView,
-                attribute: .width,
-                relatedBy: .equal,
-                toItem: self.view,
-                attribute: .width,
-                multiplier: 1.0,
-                constant: 0.0),
-            NSLayoutConstraint(item: mainStackView,
-                attribute: .top,
-                relatedBy: .equal,
-                toItem: self.view,
-                attribute: .top,
-                multiplier: 1.0,
-                constant: 0.0),
-            NSLayoutConstraint(item: mainStackView,
-                attribute: .bottom,
-                relatedBy: .equal,
-                toItem: self.view,
-                attribute: .bottom,
-                multiplier: 1.0,
-                constant: 0.0)])
-        
         mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 2).isActive = true
         mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 2).isActive = true
         mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -2).isActive = true
@@ -85,7 +39,6 @@ class KeyboardViewController: UIInputViewController {
     }
     
     override func viewWillLayoutSubviews() {
-        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
         super.viewWillLayoutSubviews()
     }
     
@@ -95,42 +48,9 @@ class KeyboardViewController: UIInputViewController {
     
     override func textDidChange(_ textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
     }
     
     func addKeyboardButtons() -> UIScrollView {
-//        let scrollView = UIScrollView()
-//        scrollView.alwaysBounceHorizontal = true
-//        scrollView.bounces = true
-//        scrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.width)
-//
-//        for (index, item) in ["A", "B", "C", "D", "E"].enumerated() {
-//            let button = UIButton(type: .system)
-//            button.setTitle(item, for: .normal)
-//            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-//            button.backgroundColor = .white
-//            button.translatesAutoresizingMaskIntoConstraints = false
-//            button.addTarget(self, action: #selector(didTapButton(sender:)), for: .touchUpInside)
-//
-//            button.widthAnchor.constraint(equalToConstant: 80.0).isActive = true
-//            button.heightAnchor.constraint(equalToConstant: 80.0).isActive = true
-//
-//            let wrapperView = UIView()
-//            wrapperView.frame = CGRectMake(100.0 * CGFloat(index), 0, 100.0, 100.0)
-//            wrapperView.addSubview(button)
-//
-//            wrapperView.widthAnchor.constraint(equalToConstant: 100.0).isActive = true
-//            wrapperView.heightAnchor.constraint(equalToConstant: 100.0).isActive = true
-//            scrollView.addSubview(wrapperView)
-//        }
         let scrollView = UIScrollView()
         let numberOfSubViews = 5
         
@@ -181,13 +101,13 @@ class KeyboardViewController: UIInputViewController {
             view.backgroundColor = colorsArray[Int(arc4random_uniform(UInt32(colorsArray.count)))] as UIColor
             view.translatesAutoresizingMaskIntoConstraints = false
             
-//            let button = UIButton(type: .system)
-//            button.setTitle("\(item)", for: .normal)
-//            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-//            button.backgroundColor = .white
-//            button.translatesAutoresizingMaskIntoConstraints = false
-//            button.addTarget(self, action: #selector(didTapButton(sender:)), for: .touchUpInside)
-//            view.addSubview(button)
+            let button = UIButton(type: .system)
+            button.setTitle("\(item)", for: .normal)
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+            button.titleLabel?.lineBreakMode = .byWordWrapping
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.addTarget(self, action: #selector(didTapButton(sender:)), for: .touchUpInside)
+            view.addSubview(button)
             
             contentView.addSubview(view)
             
